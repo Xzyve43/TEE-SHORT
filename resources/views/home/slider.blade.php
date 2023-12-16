246dee951bc21913c1aade2af6b3efdb54984de6
/ Xzyve <urreamichael43@gmail.com>
2023-12-16 22:10:47 +0800

    Updated slider.blade.php with modifications

--- resources/views/home/slider.blade.php
+++ resources/views/home/slider.blade.php
@@ -11,10 +11,10 @@
                               <div class="detail-box">
                                  <h1>
                                     <span>
-                                    Sale 20% Off
+                                    UPDATED
                                     </span>
                                     <br>
-                                    On Everything
+                                    PROJECT
                                  </h1>
                                  <p>
                                     Explicabo esse amet tempora quibusdam laudantium, laborum eaque magnam fugiat hic? Esse dicta aliquid error repudiandae earum suscipit fugiat molestias, veniam, vel architecto veritatis delectus repellat modi impedit sequi.
