ce8c21dfa4b3f87dadb99626419bb55452dd4978
/ Xzyve <urreamichael43@gmail.com>
2023-12-18 14:38:03 +0800

    Title

--- resources/views/home/contact.blade.php
+++ resources/views/home/contact.blade.php
@@ -11,7 +11,7 @@
       <meta name="description" content="" />
       <meta name="author" content="" />
       <link rel="shortcut icon" href="images/favicon.png" type="">
-      <title>Tee - Short</title>
+      <title>Tee-Short</title>
       <!-- bootstrap core css -->
       <link rel="stylesheet" type="text/css" href="{{asset('home/css/bootstrap.css')}}" />
       <!-- font awesome style -->
