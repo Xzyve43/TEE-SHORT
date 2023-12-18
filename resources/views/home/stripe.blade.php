a5700fd744a67e2b00e4e9b9b7aab8d2039cc94c
/ Xzyve <urreamichael43@gmail.com>
2023-12-18 14:50:35 +0800

    fix payment

--- resources/views/home/stripe.blade.php
+++ resources/views/home/stripe.blade.php
@@ -189,6 +189,8 @@ $(function() {
      
 });
 
+</script>
+
 <script src="home/js/jquery-3.4.1.min.js"></script>
       <!-- popper js -->
       <script src="home/js/popper.min.js"></script>
@@ -196,5 +198,5 @@ $(function() {
       <script src="home/js/bootstrap.js"></script>
       <!-- custom js -->
       <script src="home/js/custom.js"></script>
-</script>
+
 </html>
